bf5b017ea550b83945a439707b3c74c16618c6bd
yiyiz11 <18652206218@163.com>
2025-04-11 20:17:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.yiyiz.link" + CUSTOM_DOMAIN]: dockerHub,
+  ["quay.yiyiz.link" + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr.yiyiz.link" + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr.yiyiz.link" + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s.yiyiz.link" + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr.yiyiz.link" + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith.yiyiz.link" + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr.yiyiz.link" + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.yiyiz.link" + CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
